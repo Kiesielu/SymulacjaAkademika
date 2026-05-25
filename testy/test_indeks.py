d1c87c5ca8ed2bbf482af87bb804c430ffdf464f
Kiesielu <pwyrwinski42@gmail.com>
2026-05-25 23:19:47 +0200

    Rozbudowa testu jednostkowego

--- testy/test_indeks.py
+++ testy/test_indeks.py
@@ -1,3 +1,4 @@
+import indeks
 from indeks import Indeks
 
 def test_indeks() -> None:
@@ -16,6 +17,15 @@ def test_indeks() -> None:
     indeks.dodaj_ocene(3.0)
 
     assert indeks.pokaz_srednia() == 3.0
+
+def test_indeks2()-> None:
+    indeks = Indeks()
+
+    indeks.dodaj_ocene(4.0)
+    indeks.dodaj_ocene(5.0)
+    indeks.dodaj_ocene(5.0)
+
+    assert round(indeks.pokaz_srednia(), 1) == 4.7
 
 def test_pusty() -> None:
     indeks = Indeks()
