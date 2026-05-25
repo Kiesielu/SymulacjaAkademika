cec08b3be520699f9c3748df98d448334fe18b39
Kiesielu <pwyrwinski42@gmail.com>
2026-05-25 23:11:39 +0200

    Test jednostkowy dla indeks.py

--- testy/test_indeks.py
+++ testy/test_indeks.py
@@ -8,3 +8,15 @@ def test_indeks() -> None:
     indeks.dodaj_ocene(5.0)
 
     assert indeks.pokaz_srednia() == 4.0
+
+    indeks.dodaj_ocene(2.0)
+    indeks.dodaj_ocene(2.0)
+    indeks.dodaj_ocene(2.0)
+    indeks.dodaj_ocene(3.0)
+    indeks.dodaj_ocene(3.0)
+
+    assert indeks.pokaz_srednia() == 3.0
+
+def test_pusty() -> None:
+    indeks = Indeks()
+    assert indeks.pokaz_srednia() == 0.0
