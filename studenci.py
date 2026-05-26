226f140c71d14f71ed766ab08c81c34b36adc8ae
Kiesielu <pwyrwinski42@gmail.com>
2026-05-26 23:37:55 +0200

    test jednostkowy studenci.py

--- studenci.py
+++ studenci.py
@@ -6,6 +6,7 @@ class Student:
         stud.imie: str = imie  # disclaimer: imiona zrobimy przez typera
         stud.wiedza: int = 10  # start 10 do max 100
         stud.stres: int = 0  # start 0 do max 100
+        stud.spoleczny: int = 50
         stud.indeks: Indeks = Indeks()  # do nadania mu indeksu
 
     # def logi(stud) -> None:
@@ -14,24 +15,26 @@ class Student:
     def ucz_sie(stud) -> None:
         stud.wiedza += 10
         stud.stres += 5
+        stud.spoleczny += 5
 
 
 class Kujon(Student):
     def ucz_sie(stud) -> None:
         stud.wiedza += 20
         stud.stres += 10
-
+        stud.spoleczny -= 5 #wiadomo siedzi w domu i sie tylko uczy hehe
 
 class Imprezowicz(Student):
     def ucz_sie(stud) -> None:
         stud.wiedza += 2
         stud.stres += 2
-
+        stud.spoleczny += 15
 
 class Tancerz(Student):
     def ucz_sie(stud) -> None:
         stud.wiedza += 5
         stud.stres += 1
+        stud.spoleczny += 10
 
 
 class Gawendziarz(Student):
---
+++ testy/test_studenci.py
@@ -0,0 +1,50 @@
+from studenci import Kujon, Imprezowicz, Tancerz, Gawendziarz
+
+def test_student_kujon() -> None:
+    kujon = Kujon("Olgierd")
+    imprezowicz = Imprezowicz("Zbyszek")
+    tancerz = Tancerz("Anna")
+    gawendziarz = Gawendziarz("Jan")
+
+    kujon.ucz_sie()
+    assert kujon.wiedza == 30
+    assert kujon.stres == 10
+    assert kujon.spoleczny == 45
+    kujon.ucz_sie()
+    assert kujon.wiedza == 50
+    assert kujon.stres == 20
+    assert kujon.spoleczny == 40
+
+    imprezowicz.ucz_sie()
+    assert imprezowicz.wiedza == 12
+    assert imprezowicz.stres == 2
+    assert imprezowicz.spoleczny == 65
+    imprezowicz.ucz_sie()
+    assert imprezowicz.wiedza == 14
+    assert imprezowicz.stres == 4
+    assert imprezowicz.spoleczny == 80
+
+    tancerz.ucz_sie()
+    assert tancerz.wiedza == 15
+    assert tancerz.stres == 1
+    assert tancerz.spoleczny == 60
+    tancerz.ucz_sie()
+    assert tancerz.wiedza == 20
+    assert tancerz.stres == 2
+    assert tancerz.spoleczny == 70
+
+    gawendziarz.ucz_sie()
+    assert gawendziarz.wiedza == 20
+    assert gawendziarz.stres == 5
+    assert gawendziarz.spoleczny == 55
+    gawendziarz.ucz_sie()
+    assert gawendziarz.wiedza == 30
+    assert gawendziarz.stres == 10
+    assert gawendziarz.spoleczny == 60
+
+
+
+
+
+
+
