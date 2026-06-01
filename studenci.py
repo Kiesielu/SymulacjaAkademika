665f1b4403dedb2826e99bc00872e86e6cb47b23
Kiesielu <pwyrwinski42@gmail.com>
2026-06-01 16:27:34 +0200

    dodanie mechanizmu z seedem

--- studenci.py
+++ studenci.py
@@ -1,12 +1,18 @@
 from indeks import Indeks
 
+def inicjalizuj_gre(podany_seed=None) -> None:
+    if podany_seed is not None:
+        random.seed(podany_seed)
+    else:
+        random.seed()
 
 class Student:
     def __init__(stud, imie: str) -> None:
         stud.imie: str = imie  # disclaimer: imiona zrobimy przez typera
-        stud.wiedza: int = 10  # start 10 do max 100
-        stud.stres: int = 0  # start 0 do max 100
-        stud.spoleczny: int = 50
+
+        stud.wiedza: int = random.randint(10, 30)  # start 10 do max 100
+        stud.stres: int = random.randint(0, 20)  # start 0 do max 100
+        stud.spoleczny: int = random.randint(10, 40)
         stud.indeks: Indeks = Indeks()  # do nadania mu indeksu
 
     # def logi(stud) -> None:
